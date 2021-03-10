7edc17d25e93532773c4597d99db73710497195b
Dan <qqwwee08@naver.com>
2021-03-10 22:57:26 +0900

    ie promise polyfill 추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,9 +3,12 @@ import styled from "styled-components";
 import themes from "@/styles/themes";
 
 const MainPage = () => {
-  return <__Wrapper>asd12124141241555555555555555424</__Wrapper>;
+  return (
+    <__Wrapper>
+      asd12124141241555555555555555424
+      <img src="/src/images/logo512.png" />
+    </__Wrapper>
+  );
 };
-const __Wrapper = styled.div`
-  background: ${themes.color.black};
-`;
+const __Wrapper = styled.div``;
 export default MainPage;
