78bbe6f6206b28956243f4db25ef97e959f6e013
shindaehyun <qqwwee08@naver.com>
2021-06-27 12:11:23 +0900

    add dotenv-webpack 추가

--- config/webpack/common.js
+++ config/webpack/common.js
@@ -6,7 +6,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "index_bundle.js",
+      filename: "index_bundle.js",
     publicPath: "/",
   },
   resolve: {
